b67c91072caaa4e2f3bf243f2432d1dd2f96ff27
Ankuristic <63995112+Ankuristic@users.noreply.github.com>
2024-02-26 11:09:46 +0530

    second commit

---
+++ node-socket/config.js
@@ -0,0 +1,12 @@
+// import mongoose from "mongoose";
+
+
+// export const connect = async () =>{
+
+//     await mongoose.connect();
+
+//     useNewUrlParser:true,
+//     useUnifiedTopology:true
+
+
+// }
